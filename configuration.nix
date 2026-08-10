a2a5e630b694c0ff9644905b842dd771ba11cf74
Oleksii Orlenko <alex@aqrln.net>
2026-08-11 00:58:18 +0200

    Configure tlp

--- configuration.nix
+++ configuration.nix
@@ -45,6 +45,21 @@
     pulse.enable = true;
   };
 
+  services.power-profiles-daemon.enable = false;
+
+  services.tlp = {
+    enable = true;
+    pd.enable = true;
+    settings = {
+      CPU_DRIVER_OPMODE_ON_AC = "active";
+      CPU_DRIVER_OPMODE_ON_BAT = "active";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+    };
+  };
+
   users.users.aqrln = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
