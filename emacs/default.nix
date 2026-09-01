dd6103d24d5947ec60af9743d1822ccc55dad107
Oleksii Orlenko <alex@aqrln.net>
2026-09-01 19:33:33 +0200

    emacs: update codex-acp to 1.7.0

--- emacs/default.nix
+++ emacs/default.nix
@@ -3,16 +3,16 @@
 let
   codex-acp = pkgs.buildNpmPackage rec {
     pname = "agentclientprotocol-codex-acp";
-    version = "1.6.2";
+    version = "1.7.0";
 
     src = pkgs.fetchFromGitHub {
       owner = "agentclientprotocol";
       repo = "codex-acp";
       rev = "v${version}";
-      hash = "sha256-QNQ9x4CEO6xzKDd1vggBbntnGLjI1TBmg5ydCWM3T7k=";
+      hash = "sha256-oOByalquD4I4s+3JafMDYlQ3dGN1TAfq3sy6owSsv6M=";
     };
 
-    npmDepsHash = "sha256-uK03isdvl9tpYDF1sapHjmPdhtLGbdjE3cDU/qFa5G0=";
+    npmDepsHash = "sha256-5dk7J0nDg4YWpiSnnY11JPWKgMgJn1Wi0KGAyhdc1Fk=";
 
     nativeBuildInputs = [ pkgs.makeWrapper ];
 
